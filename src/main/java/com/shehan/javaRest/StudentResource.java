702b337669e4a3922d91af348b8722ad76f5c368
shehan82 <shehansandeepa82@gmail.com>
2020-12-31 08:00:31 +0530

    return list of students

--- src/main/java/com/shehan/javaRest/StudentResource.java
+++ src/main/java/com/shehan/javaRest/StudentResource.java
@@ -1,5 +1,8 @@
 package com.shehan.javaRest;
 
+import java.util.Arrays;
+import java.util.List;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -10,13 +13,19 @@ public class StudentResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Student getStudent()
+	public List<Student> getStudent()
 	{
 		Student s1 = new Student();
 		s1.setName("shehan");
 		s1.setAge(23);
 		
-		return s1;
+		Student s2 = new Student();
+		s2.setName("Sandeepa");
+		s2.setAge(21);
+		
+		List<Student> studentList = Arrays.asList(s1,s2);
+		
+		return studentList;
 	}
 
 }
