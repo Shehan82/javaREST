1184f9bae4f3b5fc32f9d754ee5e437e8972dff3
shehan82 <shehansandeepa82@gmail.com>
2020-12-31 08:34:39 +0530

    create studentRepository class

---
+++ src/main/java/com/shehan/javaRest/StudentRepository.java
@@ -0,0 +1,25 @@
+package com.shehan.javaRest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentRepository {
+	
+	List<Student> students;
+	
+	public StudentRepository()
+	{
+		students = new ArrayList<>();
+		
+		Student s1 = new Student();
+		s1.setName("shehan");
+		s1.setAge(23);
+		
+		Student s2 = new Student();
+		s2.setName("Sandeepa");
+		s2.setAge(21);
+		
+		students.add(s1);
+		students.add(s2);
+	}
+}
